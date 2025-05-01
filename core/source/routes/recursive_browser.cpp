7d893491efbaae9a5541e566a5de1c6e6e3231c2
Leo Victor Cruz Vasconcelos <leotpw9@gmail.com>
2025-05-01 18:02:16 -0300

    Refactor recursive_browser

--- core/source/routes/recursive_browser.cpp
+++ core/source/routes/recursive_browser.cpp
@@ -8,46 +8,28 @@ void recursive_browser(const httplib::Request& req, httplib::Response& res) {
     fs::path path = fs::weakly_canonical(fs::path(".") / rel_path);
     fs::path shared_path = fs::weakly_canonical(fs::path(".") / shared_directory);
 
-    std::size_t size = shared_path.string().size();
-    std::string subpath = path.string().substr(0,size); 
-    
-    if(subpath != shared_path.string()) {
+    std::size_t size        = shared_path.string().size();
+    std::string prefix_path = path.string().substr(0,size);
+    std::string sub_path    = path.string().substr(size+1);
+
+    if(prefix_path != shared_path.string()) {
         res.status = 403;
         res.set_content("Forbidden","text/plain");
         return;
     }
 
-    if(!recursive_mode) {
-        if(validate_file(path.string())) {
-            send_file(res,path);
-        } else {
-            res.status = 404;
-            res.set_content("File Not Found", "text/plain");
-            return;
-        }
-    
-    } else {
-
-        if (!fs::exists(path)) {
-            res.status = 404;
-            res.set_content("Not Found", "text/plain");
-            return;
-        }
-
-        if (fs::is_directory(path)) {
-            std::string html = "<html><body><h1>Directory Listing</h1><ul>";
-            for (const auto& entry : fs::directory_iterator(path)) {
-                html += "<li><a href='/files/" + (rel_path + "/" + entry.path().filename().string()) + "'>" + entry.path().filename().string() + "</a></li>";
-            }
-            html += "</ul></body></html>";
-            res.set_content(html, "text/html");
-        } else if (fs::is_regular_file(path)) {
-            send_file(res,path);
-        } else {
-            res.status = 403;
-            res.set_content("Forbidden", "text/plain");
-            return;
-        }
-
+    if(sub_path.find("/") != std::string::npos && !recursive_mode) {
+        res.status = 403;
+        res.set_content("Forbidden","text/plain");
+        return;
     }
+
+    if(!validate_file(path.string())) {
+        res.status = 404;
+        res.set_content("File Not Found", "text/plain");
+        return;
+    }
+
+    send_file(res,path);
+    return;
 }
