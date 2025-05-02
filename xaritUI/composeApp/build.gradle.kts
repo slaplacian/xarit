f8b3442d525a057dd8c93f2022d0bada39ad53d7
williams-bruce <williamsbruceprep@gmail.com>
2025-05-02 10:38:39 -0300

    adding xaritUI to project

---
+++ xaritUI/composeApp/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+
+    kotlin("plugin.serialization") version "2.0.21"
+}
+
+kotlin {
+    jvm("desktop")
+    
+    sourceSets {
+        val desktopMain by getting
+        
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.8.0-alpha10")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+            implementation("io.insert-koin:koin-core:4.1.0-Beta5")
+            implementation("io.insert-koin:koin-compose:4.1.0-Beta5")
+            implementation("io.insert-koin:koin-compose-viewmodel:4.1.0-Beta5")
+            implementation("io.insert-koin:koin-compose-viewmodel-navigation:4.1.0-Beta5")
+        }
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutines.swing)
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.xarit.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.xarit"
+            packageVersion = "1.0.0"
+        }
+    }
+}
